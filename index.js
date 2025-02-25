3ea54411fda67f4b42b94b59f1fe7d0975d7c251
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-02-25 16:48:07 -0500

    part1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -30,10 +30,10 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log("The four numbers are valid according to the provided criteria  - " + isValid);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -43,3 +43,24 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+  //Checking if all numbers is divisible by 5
+
+  const divby5 = (n1%5) + (n2%5) + (n3%5) + (n4%5) == 0;
+  console.log("Checking if all numbers is divisible by 5  - " + divby5);
+
+  //Checking if first number is larger than last
+
+  const checkFirstLast = n1 > n4 ;
+  console.log("Checking if the first number is larger than the last number -  " + checkFirstLast);
+
+  //Arithmetic chain
+
+  let calculate = n2 - n1;
+  calculate *= n3;
+  calculate %= n4;
+
+  console.log("The reminder after performing the arithmetic chain  - " + calculate);
+
+  
